2484317d26916853aa45b76c75c8566fafd24e2d
Prince Vishwakarma <pko62547@gmail.com>
2025-04-01 19:27:07 +0530

    mongodb

--- backend/config/connectionDB.js
+++ backend/config/connectionDB.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB Connected successfully");
   } catch (error) {
     console.error(`Error: ${error.message}`);
