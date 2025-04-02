2d3a5be469037ff9dcc86fa48853ad4df665645e
Prince Vishwakarma <pko62547@gmail.com>
2025-04-02 08:43:14 +0530

    final commit for production

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/connectionDB.js";
 import userRoutes from "./routes/user.routes.js";
 import blogRoutes from "./routes/blog.routes.js";
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 // middlewares
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+// static files
 // API ENDPOINTS
 app.use("/images", express.static("uploads"));
 app.use("/user", userRoutes);
